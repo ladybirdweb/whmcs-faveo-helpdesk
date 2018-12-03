4c3f658f2fd2c14a4afd1b1dd9655d5a0f27df64
Sirhan Shafahath <sirhanshafahath@gmail.com>
2018-12-03 13:40:35 +0530

    Development Configuration for APL

--- modules/addons/faveohelpdesk/SCRIPT/apl_core_configuration.php
+++ modules/addons/faveohelpdesk/SCRIPT/apl_core_configuration.php
@@ -6,25 +6,25 @@
 
 
 //Random salt used for encryption. It should contain random symbols (16 or more recommended) and be different for each application you want to protect. Cannot be modified after installing script.
-define("APL_SALT", "9nvNVrXq2AXu9dJ");
+define("APL_SALT", "bcd665e73c694107");
 
 //The URL (without / at the end) where Auto PHP Licenser from /WEB directory is installed on your server. No matter how many applications you want to protect, a single installation is enough.
-define("APL_ROOT_URL", "https://www.demo.phpmillion.com/apl");
+define("APL_ROOT_URL", "https://license.faveohelpdesk.com");
 
 //Unique numeric ID of product that needs to be licensed. Can be obtained by going to Products > View Products tab in Auto PHP Licenser dashboard and selecting product to be licensed. At the end of URL, you will see something like products_edit.php?product_id=NUMBER, where NUMBER is unique product ID. Cannot be modified after installing script.
-define("APL_PRODUCT_ID", 1);
+define("APL_PRODUCT_ID", 38);
 
 //Time period (in days) between automatic license verifications. The lower the number, the more often license will be verified, but if many end users use your script, it can cause extra load on your server. Available values are between 1 and 365. Usually 7 or 14 days are the best choice.
 define("APL_DAYS", 7);
 
 //Place to store license signature and other details. "DATABASE" means data will be stored in MySQL database (recommended), "FILE" means data will be stored in local file. Only use "FILE" if your application doesn't support MySQL. Otherwise, "DATABASE" should always be used. Cannot be modified after installing script.
-define("APL_STORAGE", "FILE");
+define("APL_STORAGE", "DATABASE");
 
 //Name of table (will be automatically created during installation) to store license signature and other details. Only used when "APL_STORAGE" set to "DATABASE". The more "harmless" name, the better. Cannot be modified after installing script.
-define("APL_DATABASE_TABLE", "user_data");
+define("APL_DATABASE_TABLE", "faveo_license");
 
 //Name and location (relative to directory where "apl_core_configuration.php" file is located, cannot be moved outside this directory) of file to store license signature and other details. Can have ANY name and extension. The more "harmless" location and name, the better. Cannot be modified after installing script. Only used when "APL_STORAGE" set to "FILE" (file itself can be safely deleted otherwise).
-define("APL_LICENSE_FILE_LOCATION", "signature/license.key");
+define("APL_LICENSE_FILE_LOCATION", "signature/license.key.example");
 
 //Notification to be displayed when connection to server can't be established. Other notifications will be automatically fetched from server.
 define("APL_NOTIFICATION_NO_CONNECTION", "Can't connect to licensing server.");
@@ -52,7 +52,7 @@ define("APL_NOTIFICATION_BYPASS_VERIFICATION", "No need to verify");
 
 
 //Secret key used to verify if configuration file included in your script is genuine (not replaced with 3rd party files). It can contain any number of random symbols and should be different for each application you want to protect. You should also change its name from "APL_INCLUDE_KEY_CONFIG" to something else, let's say "MY_CUSTOM_SECRET_KEY"
-define("APL_INCLUDE_KEY_CONFIG", "some_random_text");
+define("APL_INCLUDE_KEY_CONFIG", "b211c019d5d48c1c");
 
 //IP address of your Auto PHP Licenser installation. If IP address is set, script will always check if "APL_ROOT_URL" resolves to this IP address (very useful against users who may try blocking or nullrouting your domain on their servers). However, use it with caution because if IP address of your server is changed in future, old installations of protected script will stop working (you will need to update this file with new IP and send updated file to end user). If you want to verify licensing server, but don't want to lock it to specific IP address, you can use APL_ROOT_NAMESERVERS option (because nameservers change is unlikely).
 define("APL_ROOT_IP", "");
@@ -61,13 +61,13 @@ define("APL_ROOT_IP", "");
 //define("APL_ROOT_NAMESERVERS", array()); //ATTENTION! THIS FEATURE ONLY WORKS WITH PHP 7.0 AND HIGHER, ONLY UNCOMMENT THIS LINE IF PROTECTED SCRIPT WILL RUN ON COMPATIBLE SERVER!
 
 //When option set to "YES", script files and MySQL data will be deleted when illegal usage is detected. This is very useful against users who may try using pirated software; if someone shares his license with 3rd parties (by sending it to a friend, posting on warez forums, etc.) and you cancel this license, Auto PHP Licenser will try to delete all script files and any data in MySQL database for everyone who uses cancelled license. For obvious reasons, data will only be deleted if license is cancelled. If license is invalid or expired, no data will be modified. Use at your own risk!
-define("APL_DELETE_CANCELLED", "");
+define("APL_DELETE_CANCELLED", "NO");
 
 //When option set to "YES", script files and MySQL data will be deleted when cracking attempt is detected. This is very useful against users who may try cracking software; if some unauthorized changes in core functions are detected, Auto PHP Licenser will try to delete all script files and any data in MySQL database. Use at your own risk!
-define("APL_DELETE_CRACKED", "YES");
+define("APL_DELETE_CRACKED", "NO");
 
 //When option set to "YES", ALL files and MySQL data will be deleted when cracking attempt is detected. This option only works when APL_DELETE_CRACKED is set to "YES". The main difference between standard (used by default when APL_DELETE_CRACKED is set to "YES") and GOD mode is that GOD mode deletes not only script files, but also all other files from user's website (including other scripts, custom user files, etc.)
-define("APL_GOD_MODE", "YES");
+define("APL_GOD_MODE", "NO");
 
 
 //-----------NOTIFICATIONS FOR DEBUGGING PURPOSES ONLY. SHOULD NEVER BE DISPLAYED TO END USER-----------//
